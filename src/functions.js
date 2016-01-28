7d99b69e8408306114c8535d235d59e6a306b29a
Francisco Calle <francisco.calle@beeva.com>
2016-01-28 14:53:18 +0100

    So easy

--- src/functions.js
+++ src/functions.js
@@ -1,33 +1,15 @@
 'use strict';
 
-function mapForEach(arr, fn) {
-  const newArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    newArr.push(
-      fn(arr[i])
-    );
-  }
-  return newArr;
-}
-function checkPastLimit(limiter) {
-  return function (l, i) {
-    return i > l;
-  }.bind(this, limiter);
-}
+const _ = require('./underscore-1.8.3');
 
 const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log('arr1', arr1);
 
-const arr2 = mapForEach(arr1, (item) => {
-  return item * 2;
+const arr2 = _.map(arr1, function(item) {
+  return item * 3;
 });
 console.log('arr2', arr2);
-
-const arr3 = mapForEach(arr1, (item) => {
-  console.log(`doing something with the item: ${item}`);
-  return item > 5;
+const arr3 = _.filter(arr1, function(item) {
+  return item % 2 === 0;
 });
 console.log('arr3', arr3);
-
-const arr4 = mapForEach(arr1, checkPastLimit(5));
-console.log('arr4', arr4);
