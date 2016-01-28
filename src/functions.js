3cbbe610a34df295f9ffdaec69f1776948b008ad
Francisco Calle <francisco.calle@beeva.com>
2016-01-28 12:55:56 +0100

    Useful but still ...

---
+++ src/functions.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function mapForEach(arr, fn) {
+  const newArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.push(
+      fn(arr[i])
+    );
+  }
+  return newArr;
+}
+
+const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log('arr1', arr1);
+
+const arr2 = mapForEach(arr1, (item) => {
+  return item * 2;
+});
+console.log('arr2', arr2);
+const arr3 = mapForEach(arr1, (item) => {
+  console.log(`doing something with the item: ${item}`);
+  return item > 5;
+});
+console.log('arr3', arr3);
