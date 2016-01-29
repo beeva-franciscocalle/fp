84ce1120fee9c4f18f041ee7e7dc425bf1db402c
Francisco Calle <francisco.calle@beeva.com>
2016-01-29 09:41:14 +0100

    Everything in ES6

--- src/functions.js
+++ src/functions.js
@@ -1,33 +1,39 @@
 'use strict';
 
-function mapForEach(arr, fn) {
+const mapForEach = (arr, fn) => {
   const newArr = [];
   for (let i = 0; i < arr1.length; i++) {
-    newArr.push(
-      fn(arr[i])
-    );
+    newArr.push( fn(arr[i]) );
   }
   return newArr;
-}
-function checkPastLimit(limiter) {
+};
+const checkPastLimit = (limiter) => {
   return function (l, i) {
     return i > l;
   }.bind(this, limiter);
-}
+};
 
+console.log('>> original array');
 const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log('arr1', arr1);
+console.log("------------o------------");
 
+console.log('>> double the values');
 const arr2 = mapForEach(arr1, (item) => {
   return item * 2;
 });
-console.log('arr2', arr2);
+console.log('>> arr2', arr2);
+console.log("------------o------------");
 
+console.log('>> mapForEach greater than 5');
 const arr3 = mapForEach(arr1, (item) => {
   console.log(`doing something with the item: ${item}`);
   return item > 5;
 });
-console.log('arr3', arr3);
+console.log('>> arr3', arr3);
+console.log("------------o------------");
 
+console.log('>> checkPastLimit', 5);
 const arr4 = mapForEach(arr1, checkPastLimit(5));
 console.log('arr4', arr4);
+console.log("------------o------------");
